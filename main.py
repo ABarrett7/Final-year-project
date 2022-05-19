264e39388e1fba359fe763c70a4d82e2335d4199
ABarrett7 <105759587+ABarrett7@users.noreply.github.com>
2022-05-19 02:05:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
-from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from flask import jsonify
 import numpy as np
-import pandas as pd
 import feature_extraction
+from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.model_selection import train_test_split
+from flask import jsonify
 
 
 def getResult(url):
 
-    #Importing dataset
+    #Dataset is imported using numpy
     data = np.loadtxt("phishing.csv", delimiter = ",")
 
     #Seperating features and labels
-    X = data[: , :-1]
-    y = data[: , -1]
+    X = data[: , 0:14]
+    y = data[: , 15]
 
-    #Seperating training features, testing features, training labels & testing labels
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-    clf = RandomForestClassifier(max_depth=32, random_state=0)
-    clf = clf.fit(X_train, Y_train)
+
+    #Making the training and testing features and labels
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    #Setting random forest features
+    clf = rfc(max_depth=32, random_state=0)
+    clf.fit(X_train, y_train)
     Y_prediction = clf.predict(X_test)
 
     from sklearn.model_selection import ShuffleSplit
 
     cv = ShuffleSplit(n_splits=5, test_size=0.2)
 
+    # Produces accuracy score of the algorithm using cross fold validation
     from sklearn.model_selection import cross_val_score
-    scores = cross_val_score(clf, X, Y, cv=cv)
-    print("Cross fold validation accuracy mean:", scores.mean())
+    scores = cross_val_score(clf, X, y, cv=cv)
+    print("Accuracy score", scores.mean())
 
     X_new = []
 
@@ -37,6 +37,7 @@ def getResult(url):
     X_new=feature_extraction.generate_data_set(X_input)
     X_new = np.array(X_new).reshape(1,-1)
 
+    #Determines whether the URL entered is phishing or legitmate
     try:
         prediction = clf.predict(X_new)
         if prediction == -1:
